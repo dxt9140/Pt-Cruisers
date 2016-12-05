4e41bd737ebb27869670839100977a3c6bbf6838
Dominick  Taylor <dxt9140@cs.rit.edu>
2016-12-05 13:46:25 -0500

    The program can now display any drivers correctly. Program onvocation now runs crrectly with very little error detection. I.e., any name entered on the command line will diplay as a racer (or not, depending on what the thread feels like doing), and the program can successfully detect if the client desires a delay.

--- pt-cruisers.c
+++ pt-cruisers.c
@@ -6,6 +6,8 @@
 #define _BSD_SOURSE
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
 #include "racer.h"
 #include "display.h"
 
@@ -13,8 +15,47 @@
 int main ( int argc, char * argv[] ) {
 
 	if ( argc < 2 ) {
-		fprintf( stderror, "ERROR" );
+		fprintf( stderr, "ERROR" );
 		return EXIT_FAILURE;
+	}
+
+	int lane = 0;
+	// Check if first argument is the max speed delay
+	long delay = strtol( arg[1], NULL, 10 );
+	if ( delay != 0 ) {
+		// first token is an int, a time delay
+		printf( "%#lx\n", delay );
+	} else {
+		// handle it as a racer
+		char * name = argv[1];
+		if ( strlen( name ) >= MAX_NAME_LEN ) {
+			fprintf( stderr, "ERROR: name must not exceed %d characters...\n", MAX_NAME_LEN );
+			return EXIT_FAILURE;
+		}
+		printf( "%d, %s\n", 1, name );
+		Racer * racer = makeRacer( name, lane );		
+		lane++;
+		pthread_t thread;
+		pthread_create( &thread, NULL, run, racer );
+	}
+
+	for ( int i = 2; i < argc; i++ ) {
+		// Do things with each of the racers
+
+		char * name = argv[i];
+		printf( "%d, %s\n", i, name );
+		if ( strlen( name ) >= MAX_NAME_LEN ) {
+			fprintf( stderr, "ERROR: name must not exceed %d characters...\n", MAX_NAME_LEN );
+			return EXIT_FAILURE;
+		}
+		
+		Racer * racer = makeRacer( name, lane );
+		lane++;
+
+		pthread_t thread;
+		pthread_create( &thread, NULL, run, racer );
+
+		// create a thread for each racer
 	}
 
 }
--- racer.c
+++ racer.c
@@ -3,18 +3,44 @@
 /// Created: 12/5/2016
 /// Implementation of the racer.h file. This program will handle the construction of the racers.
 
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 	// gotta get that strcat
+#include "racer.h"
+#include "display.h"
+
 void initRacers( long milliseconds ) {
 
 }
 
 Racer * makeRacer( char * name, int position ) {
+	Racer * racer = malloc( sizeof( Racer ) );
+	char * buff = malloc( MAX_CAR_LEN );
+	buff[0] = '~';
+	buff[1] = 'O';
+	buff[2] = '=';
+	strcat( buff, name );
+	for ( int i = 3; i < 10; i++ ) {
+		if ( buff[i] == '\0' ) {
+			buff[i] = '-';
+		}
+	}
+	buff[10] = 'o';
+	buff[11] = '>';
+	racer->graphic = buff;
+	racer->row = position;
+	racer->dist = 0;
+	return racer;
 
 }
 
 void destroyRacer( Racer * racer ) {
-
+	free( racer->graphic );
+	free( racer );
 }
 
 void * run( void * racer ) {
-
+	Racer * driver = (Racer *)racer;
+	printf( "%s\n", driver->graphic );
 }
